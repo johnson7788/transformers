2963b7683e6da3dd015515ad0a0b4f4869f939d9
johnson <johnson@github.com>
2021-01-14 12:09:58 +0800

    2021-01-14_12:09

--- myexample2/download_roberta.py
+++ myexample2/download_roberta.py
@@ -1,11 +1,30 @@
-from transformers import BertTokenizer, AlbertForMaskedLM
+from transformers import BertTokenizer, AutoModelForMaskedLM, BertModel, RobertaModel
 import os
-pretrained = 'hfl/chinese-roberta-wwm-ext'
-tokenizer = BertTokenizer.from_pretrained(pretrained)
-model = AlbertForMaskedLM.from_pretrained(pretrained)
 
-model.save_pretrained('roberta_model')
-tokenizer.save_pretrained('roberta_model')
-# os.remove("albert_model/special_tokens_map.json")
-# os.remove("albert_model/tokenizer_config.json")
-# os.system("mv albert_model ../")
+def robera():
+    pretrained = 'hfl/chinese-roberta-wwm-ext'
+    tokenizer = BertTokenizer.from_pretrained(pretrained)
+    model = AutoModelForMaskedLM.from_pretrained(pretrained)
+
+    model.save_pretrained('roberta_model')
+    tokenizer.save_pretrained('roberta_model')
+    # os.remove("albert_model/special_tokens_map.json")
+    # os.remove("albert_model/tokenizer_config.json")
+    # os.system("mv albert_model ../")
+
+def rbt3():
+    """
+    RBT3 3层RoBERTa-wwm-ext-base
+    RBTL3（3层RoBERTa-wwm-ext-base/large)
+    Returns:
+
+    """
+    pretrained = "hfl/rbt3"
+    tokenizer = BertTokenizer.from_pretrained(pretrained)
+    model = RobertaModel.from_pretrained(pretrained)
+
+    model.save_pretrained('rbt3')
+    tokenizer.save_pretrained('rbt3')
+
+if __name__ == '__main__':
+    rbt3()
