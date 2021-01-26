feee97f3801f064ab9c4998dec3bc1d08fd245b9
johnson <johnson@github.com>
2021-01-26 13:16:37 +0800

    2021-01-26_13:16

---
+++ myexample/download_albert.py
@@ -0,0 +1,12 @@
+from transformers import BertTokenizer, AlbertForMaskedLM
+import os
+pretrained = 'voidful/albert_chinese_xlarge'
+# pretrained = 'voidful/albert_chinese_large'
+tokenizer = BertTokenizer.from_pretrained(pretrained)
+model = AlbertForMaskedLM.from_pretrained(pretrained)
+
+model.save_pretrained('albert_model')
+tokenizer.save_pretrained('albert_model')
+os.remove("albert_model/special_tokens_map.json")
+os.remove("albert_model/tokenizer_config.json")
+# os.system("mv albert_model ../")
